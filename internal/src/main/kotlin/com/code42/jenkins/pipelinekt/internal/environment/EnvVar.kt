3efe96bb5cc32191740d346e26a0e093ce968ad4
Erik Rasmussen <erik.rasmussen@code42.com>
2021-05-03 15:54:38 -0600

    Fix braces around environment.

--- internal/src/main/kotlin/com/code42/jenkins/pipelinekt/internal/environment/EnvVar.kt
+++ internal/src/main/kotlin/com/code42/jenkins/pipelinekt/internal/environment/EnvVar.kt
@@ -5,6 +5,6 @@ import com.code42.jenkins.pipelinekt.core.writer.GroovyWriter
 
 data class EnvVar(val name: String, val value: String) : Environment {
     override fun toGroovy(writer: GroovyWriter) {
-        writer.writeln("$name=\"${value}\"")
+        writer.writeln("${name}=\"${value}\"")
     }
 }
